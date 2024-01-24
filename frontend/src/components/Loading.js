bc98c5f05162119cc93e55cbb9626b89ae954321
elciddelapaz <127919521+RomwelFeliciano@users.noreply.github.com>
2024-01-24 18:11:34 +0800

    Frontend: New Loading UI

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -3,9 +3,9 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="fixed flex h-96 items-center justify-center">
-      <div className="h-10 w-10 animate-spin rounded-full border-b-2 border-t-2 border-blue-400 ease-linear"></div>
-      <div className="ml-3 h-10 w-10 animate-spin rounded-full border-b-2 border-t-2 border-yellow-400 ease-linear"></div>
-      <div className="ml-3 h-10 w-10 animate-spin rounded-full border-b-2 border-t-2 border-red-400 ease-linear"></div>
+      <div className="h-10 w-10 animate-spin rounded-full border-b-8 border-t-8 border-green-400 ease-linear"></div>
+      <div className="ml-3 h-10 w-10 animate-spin rounded-full border-b-8 border-t-8 border-green-600 ease-linear"></div>
+      <div className="ml-3 h-10 w-10 animate-spin rounded-full border-b-8 border-t-8 border-green-800 ease-linear"></div>
     </div>
   );
 };
